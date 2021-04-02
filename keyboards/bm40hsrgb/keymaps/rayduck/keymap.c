3c929f27a38f8395c168cb2def5d39d789fbdd92
rayduck <ruida@dingansi.ch>
2021-04-02 20:05:18 +0800

    fixes

--- keyboards/bm40hsrgb/keymaps/rayduck/keymap.c
+++ keyboards/bm40hsrgb/keymaps/rayduck/keymap.c
@@ -70,14 +70,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | S/:  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Numpad| Ctrl | Alt  |S/Bksp|Symbol|    Space    |Number| S/Left | Down |  Up  |Fn/Rt |
+ * |Numpad| Ctrl | Alt  |S/Bksp|Symbol|    Space    |Number|S/Left|A/Down| ^/Up  |Fn/Rt |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_mit(
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
     LGUI_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOT, LCTL_T(KC_ENT),
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC,
-    MO(_NUMPAD), KC_LCTL, KC_LALT, LSFT_T(KC_BSPC), MO(_SYMBOL), KC_SPC, MO(_NUMBER), LSFT(KC_LEFT), KC_DOWN, KC_UP, LT(_FNKEY,KC_RGHT)
+    MO(_NUMPAD), KC_LCTL, KC_LALT, LSFT_T(KC_BSPC), MO(_SYMBOL), KC_SPC, MO(_NUMBER), RSFT_T(KC_LEFT), RALT_T(KC_DOWN), RSFT_T(KC_UP), LT(_FNKEY,KC_RGHT)
 ),
 
 /* Symbol
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   1  |   4  |   5  |   6  |      |      |   +  |   -  |   =  |   "  |  \   |
+ * |      |   1  |   4  |   5  |   6  |      |      |   +  |   -  |   =  |   :  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |   `  |      |      |   7  |      |      |   0  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_NUMBER] = LAYOUT_planck_mit(
     KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
-    KC_TRNS, KC_1, KC_4, KC_5, KC_6, KC_NO, KC_NO, KC_PLUS, KC_MINS, KC_EQL, KC_DQUO, KC_BSLS,
+    KC_TRNS, KC_1, KC_4, KC_5, KC_6, KC_NO, KC_NO, KC_PLUS, KC_MINS, KC_EQL, KC_COLON, KC_BSLS,
     KC_TRNS, KC_GRAVE, KC_NO, KC_NO, KC_7, KC_NO, KC_NO, KC_0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, LSFT_T(KC_DEL), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
@@ -146,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |   _  |   |  |   `  |      |
+ * |      |      |      |      |      |      |      |      |   _  |   |  |   ;  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      | Mouse|      |      |   \  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -155,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_COMMAND] = LAYOUT_planck_mit(
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UNDS, KC_PIPE, KC_GRAVE, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UNDS, KC_PIPE, KC_SCLN, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(_MOUSE), KC_NO, KC_NO, KC_BSLS, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, TO(_ADJUST), KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO)
 
